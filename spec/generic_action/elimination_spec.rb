829d47883c7656c3e7b6663a36bedae905f1bd6a
Peter Tseng <pht24@cornell.edu>
2015-05-19 23:41:35 -0700

    spec: Test double-elimination of 1-influence
    Pretty common case I think, so gotta make sure it gets tested

--- spec/generic_action/elimination_spec.rb
+++ spec/generic_action/elimination_spec.rb
@@ -4,7 +4,7 @@ require 'rebellion_g54/game'
 
 RSpec.describe RebellionG54::Game do
   context 'in a three-player game' do
-    let(:game) { example_game(3, coins: 7) }
+    let(:game) { example_game(3, coins: 7, rigged_roles: :guerrilla) }
     let(:users) { game.users }
     let!(:u1) { users[0] }
     let!(:u2) { users[1] }
@@ -24,6 +24,32 @@ RSpec.describe RebellionG54::Game do
 
       it 'does not declare the game over yet' do
         expect(game.winner).to be_nil
+      end
+    end
+
+    context 'when doubly-eliminating a 1-influence player' do
+      before(:each) do
+        game.take_choice(u1, 'coup', u3)
+        game.take_choice(u3, 'lose1')
+        game.take_choice(u2, 'guerrilla', u3)
+        game.take_choice(u3, 'challenge')
+        game.take_choice(u2, 'show1')
+      end
+
+      it 'removes the player' do
+        expect(game.size).to be == 2
+      end
+
+      it 'does not declare the game over yet' do
+        expect(game.winner).to be_nil
+      end
+
+      it 'ends the turn' do
+        expect(game.current_user).to_not be == u2
+      end
+
+      it 'skips the dead player in turn order' do
+        expect(game.current_user).to_not be == u3
       end
     end
   end
