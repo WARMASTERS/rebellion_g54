0a05d0b1f9a9cf06f760c35345ae23769d1d59f1
Peter Tseng <pht24@cornell.edu>
2015-08-21 20:55:53 -0700

    Choice: add availability

--- lib/rebellion_g54/choice.rb
+++ lib/rebellion_g54/choice.rb
@@ -1,11 +1,16 @@
 module RebellionG54; class Choice
   attr_reader :description
   attr_reader :args
+  attr_reader :available
+  attr_reader :why_unavailable
+  alias_method :available?, :available
 
   def initialize(description, args = [], &block)
     @description = description
     @args = args.freeze
     @block = block
+    @available = true
+    @why_unavailable = nil
   end
 
   def is_action?
@@ -15,6 +20,12 @@ module RebellionG54; class Choice
 
   # Expected by Decision::Base#take_choice to return [Boolean(success), String(error_message)]
   def call(args)
+    raise "#{description} is unavailable because #{@why_unavailable}" unless @available
     @block.call(args)
+  end
+
+  def unavailable!(reason)
+    @why_unavailable = reason
+    @available = false
   end
 end; end
